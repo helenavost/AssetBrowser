6ef28f29409350d9efe1e5337b91ea7cf3322b2e
Ilja Vost <antonov054@gmail.com>
2023-08-18 21:20:35 +0300

    New interation

--- addons/asset_browser/src/settings/components/component_prototype.gd
+++
@@ -1,7 +0,0 @@
-class_name AssetBrowserComponent extends VBoxContainer
-
-static var data_manager:AssetBrowserDataManager
-static var pages_manager:AssetBrowserPagesManager
-
-
-
--- addons/asset_browser/src/settings/managers/manager_prototype.gd
+++
@@ -1,1 +0,0 @@
-class_name AssetBrowser extends Node
